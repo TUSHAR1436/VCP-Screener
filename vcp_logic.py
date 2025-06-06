9ec9f53c8b01d3d61971c045c7fc9efa058c88aa
TUSHAR1436 <Kesaristock@gmail.com>
2025-06-06 16:48:59 +0530

    Update vcp_logic.py

--- vcp_logic.py
+++ vcp_logic.py
@@ -1,15 +1,11 @@
+import pandas as pd
 
-def apply_vcp_filters(df):
-    # Sample VCP logic from Chartink filters
-    df = df.copy()
-    df = df[
-        (df['atr_14'] < df['atr_14_10d_ago']) &
-        ((df['atr_14'] / df['close']) < 0.08) &
-        (df['close'] > 0.75 * df['52w_high']) &
-        (df['ema_50'] > df['ema_150']) &
-        (df['ema_150'] > df['ema_200']) &
-        (df['close'] > df['ema_50']) &
-        (df['close'] > 10) &
-        ((df['close'] * df['volume']) > 1000000)
-    ]
+def run_vcp_scan():
+    # For testing: return a dummy DataFrame
+    data = {
+        'symbol': ['RELIANCE', 'TCS', 'INFY'],
+        'close': [2500, 3200, 1500],
+        'volume': [1000000, 800000, 1200000]
+    }
+    df = pd.DataFrame(data)
     return df
