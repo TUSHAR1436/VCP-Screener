b19a3e3719c7a3a852fef62b268135e97c7b5e71
TUSHAR1436 <Kesaristock@gmail.com>
2025-06-06 17:51:47 +0530

    Update vcp_logic.py

--- vcp_logic.py
+++ vcp_logic.py
@@ -3,7 +3,7 @@ import pandas as pd
 def run_vcp_scan():
     # For testing: return a dummy DataFrame
     data = {
-        'symbol': ['RELIANCE', 'TCS', 'INFY'],
+        'symbol': ['RELIANCE', 'TCS', 'INFY'TATA MOTERS],
         'close': [2500, 3200, 1500],
         'volume': [1000000, 800000, 1200000]
     }
