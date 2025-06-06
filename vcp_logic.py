fe4d143cae3b9a605e39d80ed00485de30994efa
TUSHAR1436 <Kesaristock@gmail.com>
2025-06-06 17:50:53 +0530

    Update vcp_logic.py

--- vcp_logic.py
+++ vcp_logic.py
@@ -3,7 +3,7 @@ import pandas as pd
 def run_vcp_scan():
     # For testing: return a dummy DataFrame
     data = {
-        'symbol': ['RELIANCE', 'TCS', 'INFY' get_all_nse_symbols],
+        'symbol': ['RELIANCE', 'TCS', 'INFY'],
         'close': [2500, 3200, 1500],
         'volume': [1000000, 800000, 1200000]
     }
